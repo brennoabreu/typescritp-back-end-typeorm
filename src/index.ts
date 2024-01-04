eeb7dec3353e219396a70e3225d52bd49f7821b7
brennoabreu <brennoabreu@hotmail.com>
2024-01-04 11:15:44 -0300

    chore: sem name no banco

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('teste');
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080;
+
+const app = express()
+app.get('/', (request, response)=>{
+  response.send(`Porta em funcionamento ${PORT}`);
+})
+
+app.listen(PORT, () =>{
+  console.log(`server running in port ${PORT}`);
+})
